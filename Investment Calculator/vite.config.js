ab1a6c0b2d81035c02459769ef644db7a9cb2f2c
Khushi <khushi03rai@ggmail.com>
2025-08-26 18:09:00 +0530

    new2 commit

--- Investment Calculator/vite.config.js
+++ Investment Calculator/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ensures assets resolve correctly on Vercel
+  build: {
+    outDir: 'dist', // Vercel looks for 'dist'
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
