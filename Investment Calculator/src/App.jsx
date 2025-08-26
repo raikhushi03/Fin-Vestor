47f8e66caf6ac7659bf138e9dac86443324bb9c4
Khushi <khushi03rai@ggmail.com>
2025-08-26 18:16:43 +0530

    new4 commit

--- Investment Calculator/src/App.jsx
+++ Investment Calculator/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Header from "./components/header.jsx";
+import Header from "./components/Header.jsx";
 import UserInput from "./Components/UserInput.jsx";
 import Results from "./Components/Results.jsx";
 function App() {
